4b4e8d0fbfd5f25806cd08df33025b9c2a06a5bc
ezra365 <ezrasharpe@outlook.com>
2024-04-25 18:21:55 -0400

    data now in sqlite

---
+++ script.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sqlite3
+
+# Read data from CSV file
+csv_file = '/Users/Ezra/Documents/CS50 final project/CS50data.csv'  # Update with the path to your CSV file
+df = pd.read_csv(csv_file)
+
+print("hello")
+
+# Create a new SQLite database
+conn = sqlite3.connect('truthify.db')
+
+# Write data to SQLite database
+df.to_sql('data', conn, if_exists='replace', index=False)
+
+# Print first couple rows
+print(df.head())
+
+# Close connection
+conn.close()
+
+
+
