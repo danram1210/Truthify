d4424cfa6b14239d7084856ca642e9030ae5fc40
Armando Patino <apatino@dhcp-10-250-253-150.harvard.edu>
2024-04-30 17:10:06 -0400

    HTML

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 # Read data from CSV file
-csv_file = '/Users/Ezra/Documents/CS50 final project/CS50data.csv'  # Update with the path to your CSV file
+csv_file = '/Users/apatino/truthify/Truthify/Truthify/disinformation_domains_clean.csv'  # Update with the path to your CSV file
 df = pd.read_csv(csv_file)
 
 # Create a new SQLite database
