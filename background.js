178fd21522db0028f7da915ae85056b48d189eeb
Daniel <ramired1210@gmail.com>
2024-04-30 16:36:52 -0400

    URL reader

--- background.js
+++ background.js
@@ -3,3 +3,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       code: 'document.body.style.backgroundColor="red"'
     });
   });
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var url = tabs[0].url;
+    console.log(url);
+});
