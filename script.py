b1ebe01821f4cb9fdef21e3b3d6c562aaed3f7ab
ezra365 <ezrasharpe@outlook.com>
2024-05-01 14:05:34 -0400

    url log

--- script.py
+++ script.py
@@ -2,8 +2,10 @@ import pandas as pd
 import sqlite3
 
 # Read data from CSV file
-csv_file = '/Users/Ezra/Documents/CS50 final project/CS50data.csv'  # Update with the path to your CSV file
+csv_file = '/Users/Ezra/Documents/CS50/Truthify/CS50data.csv'  # Update with the path to your CSV file
 df = pd.read_csv(csv_file)
+
+print("hello")
 
 # Create a new SQLite database
 conn = sqlite3.connect('truthify.db')
@@ -13,6 +15,9 @@ df.to_sql('data', conn, if_exists='replace', index=False)
 
 # Close connection
 conn.close()
+
+
+
 
 
 
