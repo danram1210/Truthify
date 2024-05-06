c7db1770bfaef98c470a9e6cdbbe03902ff2f8f6
Daniel <ramired1210@gmail.com>
2024-05-05 22:11:23 -0400

    update script.py

--- script.py
+++ script.py
@@ -2,10 +2,7 @@ import pandas as pd
 import sqlite3
 
 # Read data from CSV file
-<<<<<<< HEAD
-csv_file = '/Users/Ezra/Documents/CS50/Truthify/CS50data.csv'  # Update with the path to your CSV file
-=======
-csv_file = '/Users/apatino/truthify/Truthify/Truthify/disinformation_domains_clean.csv'  # Update with the path to your CSV file
+csv_file = '/Users/Dan/Truthify/disinformation_domains_clean.csv'  # Update with the path to your CSV file
 df = pd.read_csv(csv_file)
 
 print("hello")
