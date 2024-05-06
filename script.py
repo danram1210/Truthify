85296f895394b68172bb656965c672454c68d6fd
Daniel <ramired1210@gmail.com>
2024-05-05 22:13:11 -0400

    update script.py

--- script.py
+++ script.py
@@ -2,10 +2,10 @@ import pandas as pd
 import sqlite3
 
 # Read data from CSV file
-csv_file = '/Users/Dan/Truthify/disinformation_domains_clean.csv'  # Update with the path to your CSV file
+csv_file = '/~path~/Truthify/disinformation_domains_clean.csv'  # Update with the path to your CSV file
 df = pd.read_csv(csv_file)
 
-print("hello")
+
 
 # Create a new SQLite database
 conn = sqlite3.connect('truthify.db')
